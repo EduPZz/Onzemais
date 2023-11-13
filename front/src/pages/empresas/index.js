379409b89cd7a1c5803554e625813a6cd6d8a193
EduPZz <eduardo.pozzogood@hotmail.com>
2023-11-12 22:31:09 -0300

    Fixing empresas list

--- front/src/pages/empresas/index.js
+++ front/src/pages/empresas/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import { EnvironmentOutlined } from '@ant-design/icons';
 
-import { Card, Space } from "antd";
+import { Card, List, Space } from "antd";
 import { Link } from "react-router-dom";
 
 import api from "../../services/api";
@@ -27,30 +27,29 @@ export default function Empresas() {
   return (
     <>
       {empresas.length >= 0 ? (
-        <>
-        {empresas.map((empresa) => (
-          <React.Fragment key={empresa.id}>
-            <Space direction="vertical" size={16}>
-              <Card
-                title={empresa.nome}
-                extra={
-                <Link to={`/empresas/${empresa.id}`}>
-                  Mais
-                </Link>
-                }
-                style={{
-                  width: 300,
-                }}
-              >
-                <div>
-                  <EnvironmentOutlined />
-                  {empresaEnderecoBulder(empresa)}
-                </div>
-              </Card>
-            </Space>
-          </React.Fragment>
-        ))}
-        </>
+          <List
+            grid={{ gutter: 16, column: 5 }}
+            dataSource={empresas}
+            renderItem={(empresa) => (
+              <List.Item>
+                <Card
+                  title={empresa.nome}
+                  extra={
+                  <Link to={`/empresas/${empresa.id}`}>
+                    Mais
+                  </Link>
+                  }
+                  style={{
+                    width: 300,
+                  }}
+                >
+                  <div>
+                    <EnvironmentOutlined />
+                    {empresaEnderecoBulder(empresa)}
+                  </div>
+                </Card>
+              </List.Item>)}
+          />  
       ) : (
         <h1>Nenhuma empresa cadastrada!</h1>
       )}
