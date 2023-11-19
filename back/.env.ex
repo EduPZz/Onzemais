9c6a749e430b484c41ebccde0c55a52ea6db614d
EduPZz <eduardo.pozzogood@hotmail.com>
2023-11-19 14:57:42 -0300

    Updating .env.ex

--- back/.env.ex
+++ back/.env.ex
@@ -3,6 +3,6 @@ DATABASE_URL="mysql://user:pass@localhost:3306/onzemais"
 JWT_ACCESS_SECRET="uuidgen"
 
 LOG_API="https://api_url/"
-LOG_TEAM="team"
+LOG_TEAM="4SI_GBD_09"
 LOG_PASSWORD="team_password"
 LOG_DBNAME="onzemais"
